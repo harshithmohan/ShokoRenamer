0fa02ebfd60844b87eb063cf73c89e0d4656e66b
Harshith Mohan <gharshitmohan@ymail.com>
2021-01-06 19:44:16 +0530

    Update to work with the updated renamer system

--- Renamer.cs
+++ Renamer.cs
@@ -2,152 +2,131 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text.RegularExpressions;
 using NLog;
 using Shoko.Plugin.Abstractions;
+using Shoko.Plugin.Abstractions.Attributes;
 using Shoko.Plugin.Abstractions.DataModels;
 
 namespace Shoko.Plugin.Renamer
 {
+    [Renamer("CustomRenamer", Description = "My custom renamer")]
     public class Renamer : IRenamer
     {
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        // Gets the current filename of the DLL (simplified)
-        public string Name => Assembly.GetExecutingAssembly().GetName().Name;
-
-        public void Load()
+        public string GetFilename(RenameEventArgs args)
         {
-            // ignore. We are a renamer
-        }
+            // Get the Anime Info
+            IAnime animeInfo = args.AnimeInfo.FirstOrDefault();
+            
+            // Get the preferred title (Overriden, as shown in Desktop)
+            string animeName = animeInfo?.PreferredTitle;
+            
+            // Filenames must be consistent (because OCD), so cancel and return if we can't make a consistent filename style
+            if (string.IsNullOrEmpty(animeName))
+            {
+                throw new Exception("Error in renamer: Anime name not found!");
+            }
+            Logger.Info($"Anime Name: {animeName}");
 
-        public void OnSettingsLoaded(IPluginSettings settings)
-        {
-            // Save this for later.
-            // Settings = settings as Settings;
-        }
+            // Get the episode info
+            IList<IEpisode> allEpisodesInfo = args.EpisodeInfo;
+            IEpisode firstEpisodeInfo = allEpisodesInfo.First();
+            allEpisodesInfo.RemoveAt(0);
 
-        public void GetFilename(RenameEventArgs args)
-        {
+            string episodeTitle = null;
+            string episodeTitleOrNumber = null;
+            
+            if (animeInfo.Type == AnimeType.Movie || firstEpisodeInfo.Type != EpisodeType.Episode)
+            {
+                episodeTitle = firstEpisodeInfo.Titles.FirstOrDefault(title =>
+                    title.Language == TitleLanguage.English)?.Title;
+                
+                foreach (IEpisode otherEpisodeInfo in allEpisodesInfo)
+                {
+                    episodeTitle += ", " + otherEpisodeInfo.Titles.FirstOrDefault(title =>
+                        title.Language == TitleLanguage.English)?.Title;
+                }
+
+                episodeTitleOrNumber = episodeTitle;
+            }
+            
+            if (animeInfo.Type != AnimeType.Movie)
+            {
+                string paddedEpisodeNumber = GetEpisodeNumber(firstEpisodeInfo, animeInfo);
+
+                foreach (IEpisode otherEpisodeInfo in allEpisodesInfo)
+                {
+                    paddedEpisodeNumber += '-' + GetEpisodeNumber(otherEpisodeInfo, animeInfo);
+                }
+                
+                // Add title if it's not of type "Episode"
+                if (firstEpisodeInfo.Type != EpisodeType.Episode)
+                {
+                    episodeTitleOrNumber = paddedEpisodeNumber + " - " + episodeTitle;
+                }
+                else
+                {
+                    episodeTitleOrNumber = paddedEpisodeNumber;
+                }
+            }
+
+            Logger.Info($"Episode Number or Title: {episodeTitleOrNumber}");
+            
+            // Get the info about the file
+            IVideoFile fileInfo = args.FileInfo;
+
+            // Get the info about the video stream from the MediaInfo
+            IVideoStream videoInfo = fileInfo.MediaInfo.Video;
+
+            // Get the resolution
+            string resolution = "";
             try
             {
-                // Get the Anime Info
-                IAnime animeInfo = args.AnimeInfo.FirstOrDefault();
-                
-                // Get the preferred title (Overriden, as shown in Desktop)
-                string animeName = animeInfo?.PreferredTitle;
-                
-                // Filenames must be consistent (because OCD), so cancel and return if we can't make a consistent filename style
-                if (string.IsNullOrEmpty(animeName))
-                {
-                    args.Cancel = true;
-                    return;
-                }
-                Logger.Info($"Anime Name: {animeName}");
-
-                // Get the episode info
-                IList<IEpisode> allEpisodesInfo = args.EpisodeInfo;
-                IEpisode firstEpisodeInfo = allEpisodesInfo.First();
-                allEpisodesInfo.RemoveAt(0);
-
-                string episodeTitle = null;
-                string episodeTitleOrNumber = null;
-                
-                if (animeInfo.Type == AnimeType.Movie || firstEpisodeInfo.Type != EpisodeType.Episode)
-                {
-                    episodeTitle = firstEpisodeInfo.Titles.FirstOrDefault(title =>
-                        title.Language == TitleLanguage.English)?.Title;
-                    
-                    foreach (IEpisode otherEpisodeInfo in allEpisodesInfo)
-                    {
-                        episodeTitle += ", " + otherEpisodeInfo.Titles.FirstOrDefault(title =>
-                            title.Language == TitleLanguage.English)?.Title;
-                    }
-
-                    episodeTitleOrNumber = episodeTitle;
-                }
-                
-                if (animeInfo.Type != AnimeType.Movie)
-                {
-                    string paddedEpisodeNumber = GetEpisodeNumber(firstEpisodeInfo, animeInfo);
-
-                    foreach (IEpisode otherEpisodeInfo in allEpisodesInfo)
-                    {
-                        paddedEpisodeNumber += '-' + GetEpisodeNumber(otherEpisodeInfo, animeInfo);
-                    }
-                    
-                    // Add title if it's not of type "Episode"
-                    if (firstEpisodeInfo.Type != EpisodeType.Episode)
-                    {
-                        episodeTitleOrNumber = paddedEpisodeNumber + " - " + episodeTitle;
-                    }
-                    else
-                    {
-                        episodeTitleOrNumber = paddedEpisodeNumber;
-                    }
-                }
-
-                Logger.Info($"Episode Number or Title: {episodeTitleOrNumber}");
-                
-                // Get the info about the file
-                IVideoFile fileInfo = args.FileInfo;
-
-                // Get the info about the video stream from the MediaInfo
-                IVideoStream videoInfo = fileInfo.MediaInfo.Video;
-
-                // Get the resolution
-                string resolution = "";
-                try
-                {
-                    resolution = videoInfo.Width.ToString() + 'x' + videoInfo.Height.ToString();
-                }
-                catch (Exception)
-                {
-                    resolution = Regex.Match(fileInfo.Filename, @"\d+x\d+").Value;
-                }
-
-                Logger.Info($"Resolution: {resolution}");
-
-                // Get the codec
-                string codec = "";
-                try
-                {
-                    codec = videoInfo.SimplifiedCodec;
-                }
-                catch (Exception)
-                {
-                    codec = fileInfo.Filename.Contains("HEVC") ? "HEVC" : "H264";
-                }
-                
-                Logger.Info($"Codec: {codec}");
-                
-                // Get the CRC hash
-                string crc = fileInfo.Hashes.CRC;
-                
-                Logger.Info($"CRC: {crc}");
-                
-                // Get the release group short name
-                string releaseGroup = fileInfo.AniDBFileInfo.ReleaseGroup.ShortName;
-                Logger.Info($"Release Group: {releaseGroup}");
-                
-                // Get the extension of the original filename, it includes the .
-                string ext = Path.GetExtension(fileInfo.Filename);
-
-                // The $ allows building a string with the squiggle brackets
-                // build a string like "Boku no Hero Academia - 04 (1920x1080 H264) (6B361564) [Hi10].mkv"
-                string result = $"{animeName} - {episodeTitleOrNumber} ({resolution} {codec}) ({crc}) [{releaseGroup}]{ext}";
-
-                // Remove invalid characters
-                result = result.ReplaceInvalidPathCharacters();
-
-                // Set the result
-                args.Result = result;
+                resolution = videoInfo.Width.ToString() + 'x' + videoInfo.Height.ToString();
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                Logger.Error(e, $"Unable to get new filename for {args.FileInfo?.Filename}");
+                resolution = Regex.Match(fileInfo.Filename, @"\d+x\d+").Value;
             }
+
+            Logger.Info($"Resolution: {resolution}");
+
+            // Get the codec
+            string codec = "";
+            try
+            {
+                codec = videoInfo.SimplifiedCodec;
+            }
+            catch (Exception)
+            {
+                codec = fileInfo.Filename.Contains("HEVC") ? "HEVC" : "H264";
+            }
+            
+            Logger.Info($"Codec: {codec}");
+            
+            // Get the CRC hash
+            string crc = fileInfo.Hashes.CRC;
+            
+            Logger.Info($"CRC: {crc}");
+            
+            // Get the release group short name
+            string releaseGroup = fileInfo.AniDBFileInfo.ReleaseGroup.ShortName;
+            Logger.Info($"Release Group: {releaseGroup}");
+            
+            // Get the extension of the original filename, it includes the .
+            string ext = Path.GetExtension(fileInfo.Filename);
+
+            // The $ allows building a string with the squiggle brackets
+            // build a string like "Boku no Hero Academia - 04 (1920x1080 H264) (6B361564) [Hi10].mkv"
+            string result = $"{animeName} - {episodeTitleOrNumber} ({resolution} {codec}) ({crc}) [{releaseGroup}]{ext}";
+
+            // Remove invalid characters
+            result = result.ReplaceInvalidPathCharacters();
+
+            // Return the result
+            return result;
         }
 
         private string GetEpisodeNumber(IEpisode episodeInfo, IAnime animeInfo)
@@ -171,26 +150,17 @@ namespace Shoko.Plugin.Renamer
             }
         }
 
-        public void GetDestination(MoveEventArgs args)
+        public (IImportFolder destination, string subfolder) GetDestination(MoveEventArgs args)
         {
-            try
-            {
-                // Note: ReplaceInvalidPathCharacters() replaces things like slashes, pluses, etc with Unicode that looks similar
+            // Note: ReplaceInvalidPathCharacters() replaces things like slashes, pluses, etc with Unicode that looks similar
 
-                // Get the first available import folder that is a drop destination
-                args.DestinationImportFolder =
-                    args.AvailableFolders.First(a => a.DropFolderType.HasFlag(DropFolderType.Destination));
+            // Get the first available import folder that is a drop destination
+            var destination = args.AvailableFolders.First(a => a.DropFolderType.HasFlag(DropFolderType.Destination));
 
-                // Get the preferred title (Overriden, as shown in Desktop)
-                string animeName = args.AnimeInfo.First().PreferredTitle.ReplaceInvalidPathCharacters();
-                
-                args.DestinationPath = animeName;
-            }
-            catch (Exception e)
-            {
-                // Log the error to Server
-                Logger.Error(e, $"Unable to get destination for {args.FileInfo?.Filename}");
-            }
+            // Get the preferred title (Overriden, as shown in Desktop)
+            var animeName = args.AnimeInfo.First().PreferredTitle.ReplaceInvalidPathCharacters();
+
+            return (destination, animeName);
         }
     }
 }
